7bf2812ea26954b95c233d9f7ee21a49f1823bc4
Arpit <arpit_jain@live.com>
2022-10-25 14:20:16 +0530

    adds auth module

---
+++ todoapp/src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
