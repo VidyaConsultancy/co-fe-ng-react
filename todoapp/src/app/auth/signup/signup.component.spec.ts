ca9a82f18fda03e12b3c65f5752fc36988ab88a9
Arpit <arpit_jain@live.com>
2022-10-27 12:28:49 +0530

    testing base setup

--- todoapp/src/app/auth/signup/signup.component.spec.ts
+++ todoapp/src/app/auth/signup/signup.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SignupComponent } from './signup.component';
 
@@ -8,6 +10,7 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule],
       declarations: [ SignupComponent ]
     })
     .compileComponents();
--- todoapp/src/app/todo/todo-detail/todo-detail.component.spec.ts
+++ todoapp/src/app/todo/todo-detail/todo-detail.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TodosComponent } from '../todos/todos.component';
 
 import { TodoDetailComponent } from './todo-detail.component';
 
@@ -8,9 +11,14 @@ describe('TodoDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoDetailComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'todos', component: TodosComponent },
+        ]),
+        HttpClientTestingModule,
+      ],
+      declarations: [TodoDetailComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoDetailComponent);
     component = fixture.componentInstance;
