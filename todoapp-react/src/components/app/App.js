43d9c8e511510cf4df2c70983debbaa9b0b35193
Arpit <arpit_jain@live.com>
2022-10-31 12:46:44 +0530

    react todo crud

--- todoapp-react/src/components/app/App.js
+++ todoapp-react/src/components/app/App.js
@@ -3,19 +3,9 @@ import { Todos, TodosComponent } from '../todos/Todos';
 import './App.css';
 
 function App() {
-  const [version, setVersion] = React.useState(1);
-
-  const updateVersion = () => {
-    setVersion(version + 1)
-  }
-
-  const getVersionString = `${version} version`;
-
   return (
     <div className="App">
-      <Todos version={version} updateVersion={updateVersion} />
-      {getVersionString}
-      <TodosComponent version={version} />
+      <Todos />
     </div>
   );
 }
--- todoapp-react/src/components/todos/Todos.jsx
+++ todoapp-react/src/components/todos/Todos.jsx
@@ -1,28 +1,63 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 
 // react hooks introduced in 16.8v
 // useState, useEffect, useCallback, useLayoutEffect, useReducer, useRef, useId
 
-export const Todos = (props) => {
-  let [title, setTitle] = React.useState('Todos');
-  const handleClick = () => {
-    setTitle('updated title');
-  }
+export const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const todoRef = React.useRef(null);
 
-  React.useEffect(() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.trim().length === 0) return false;
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, todo: newTodo, isCompleted: false },
+    ]);
+    setNewTodo('');
+  };
 
-    return () => {
-
-    }
-  }, [title]);
+  const handleOnChange = (e) => {
+    setNewTodo(e.target.value);
+  };
 
   return (
     <div className="container">
-      <h2 title={title}>
-        {title} | {props.version}
-      </h2>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={props.updateVersion}>Update Version</button>
+      <h2 title={"Todos"}>Todos</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="todo" className="sr-only">
+            What would you like to do?
+          </label>
+          <input
+            ref={todoRef}
+            value={newTodo}
+            onChange={handleOnChange}
+            type="text"
+            id="todo"
+            name="todo"
+            className="form-input"
+            placeholder="What would you like to do?"
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <ul className="todos">
+        {todos.length ? (
+          todos.map((todo) => (
+            <li key={todo.id} className="todo-item">
+              {todo.todo}
+            </li>
+          ))
+        ) : (
+          <div className="todo-zero-state">
+            You don't have any todos yet. Create one here
+          </div>
+        )}
+      </ul>
     </div>
   );
 };
@@ -45,22 +80,24 @@ export const Todos = (props) => {
 export class TodosComponent extends Component {
   constructor(props) {
     super(props);
-    this.appTitle = 'Todos';
+    this.appTitle = "Todos";
     this.state = {
-      title: 'Todos',
-      todos: ['Learn HTML'],
-    }
+      title: "Todos",
+      todos: ["Learn HTML"],
+    };
     // this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = () => {
-    this.setState({ title: 'Todos updated' });
-  }
+    this.setState({ title: "Todos updated" });
+  };
 
   render() {
     return (
       <div className="container">
-        <h2 title={this.state.title}>{this.state.title} || {this.appTitle}</h2>
+        <h2 title={this.state.title}>
+          {this.state.title} || {this.appTitle}
+        </h2>
         <button onClick={this.handleClick}>Click Me</button>
       </div>
     );
