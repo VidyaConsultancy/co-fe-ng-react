9280aa43ea5497e144570df1840f4b7e9bd5ad54
Arpit <arpit_jain@live.com>
2022-10-31 12:07:51 +0530

    react concepts

--- todoapp-react/src/components/app/App.js
+++ todoapp-react/src/components/app/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import { Todos, TodosComponent } from '../todos/Todos';
 import './App.css';
 
 function App() {
+  const [version, setVersion] = React.useState(1);
+
+  const updateVersion = () => {
+    setVersion(version + 1)
+  }
+
+  const getVersionString = `${version} version`;
+
   return (
     <div className="App">
-      <Todos />
-      <TodosComponent />
+      <Todos version={version} updateVersion={updateVersion} />
+      {getVersionString}
+      <TodosComponent version={version} />
     </div>
   );
 }
--- todoapp-react/src/components/todos/Todos.jsx
+++ todoapp-react/src/components/todos/Todos.jsx
@@ -1,24 +1,68 @@
 import React, { Component } from "react";
 
-export const Todos = () => {
-  const [title, setTitle] = React.useState('Todos');
-  return "Todos";
+// react hooks introduced in 16.8v
+// useState, useEffect, useCallback, useLayoutEffect, useReducer, useRef, useId
+
+export const Todos = (props) => {
+  let [title, setTitle] = React.useState('Todos');
+  const handleClick = () => {
+    setTitle('updated title');
+  }
+
+  React.useEffect(() => {
+
+    return () => {
+
+    }
+  }, [title]);
+
+  return (
+    <div className="container">
+      <h2 title={title}>
+        {title} | {props.version}
+      </h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={props.updateVersion}>Update Version</button>
+    </div>
+  );
 };
 
+/**
+ * Phase I Mounting -> invoked only once
+ * constructor
+ * static getDerivedStateFromProps
+ * render
+ * componentDidMount
+ * Phase II Updating
+ * static getDerivedStateFromProps
+ * shouldComponentUpdate
+ * render
+ * static getSnapshotBeforeUpdate
+ * componentDidUpdate
+ * Phase III Unmounting ->  invoked only once
+ * componentWillUnmount
+ */
 export class TodosComponent extends Component {
   constructor(props) {
     super(props);
     this.appTitle = 'Todos';
     this.state = {
-      title: 'Todos'
+      title: 'Todos',
+      todos: ['Learn HTML'],
     }
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  updateTitle() {
+  handleClick = () => {
     this.setState({ title: 'Todos updated' });
   }
 
   render() {
-    return "TodosComponent";
+    return (
+      <div className="container">
+        <h2 title={this.state.title}>{this.state.title} || {this.appTitle}</h2>
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
   }
 }
